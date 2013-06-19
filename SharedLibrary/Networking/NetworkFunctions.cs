96984068275731fb707c3635c1cc412abf71cb1d
jamesbtate <jamesbtate@gmail.com>
2013-06-19 10:35:17 -0400

    created testdriver project and started writing networking code.

---
+++ SharedLibrary/Networking/NetworkFunctions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Net;
+
+namespace SharedLibrary.Networking
+{
+    public class NetworkFunctions
+    {
+        public static TcpClient ConnectTo(IPAddress address, int port)
+        {
+            TcpClient client = new TcpClient();
+            client.Connect(address, port);
+            return client;
+        }
+    }
+}
---
+++ TestDriver/TestDriver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedLibrary.Networking;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TestDriver
+{
+    class TestDriver
+    {
+        static void Main(string[] args)
+        {
+            byte[] address = {127,127,127,127};
+            TcpClient client = NetworkFunctions.ConnectTo(new IPAddress(address), 12345);
+        }
+    }
+}
