3007227ae54a7a84c6cafa089f96a7d09419f7fa
jamesbtate <jamesbtate@gmail.com>
2013-06-23 20:41:07 -0400

    did some work in TestDriver.cs to test simultaneous open functionality behind NATs. Needs more testing for the case where one NAT is not well-behaved.

--- SharedLibrary/Network/Network.cs
+++ SharedLibrary/Network/Network.cs
@@ -11,15 +11,34 @@ namespace SharedLibrary.Network
 {
     public class Network
     {
-        public static TcpClient ConnectTo(IPAddress address, int port)
+        public static Socket BindTo(IPAddress address, int port)
         {
-            TcpClient client = new TcpClient();
-            client.Connect(address, port);
-            return client;
+            Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            socket.Bind(new IPEndPoint(address, 12345));
+            return socket;
+        }
+        public static Socket BindTo(int port)
+        {
+            return BindTo(IPAddress.Any, 12345);
+        }
+
+        public static bool ConnectTo(Socket client, IPAddress address, int port)
+        {
+            try
+            {
+                client.Connect(address, port);
+            }
+            catch (SocketException)
+            {
+                return false;
+            }
+            return true;
         }
 
         public static NetworkEvent DecodeNetworkEventHeader(byte[] bytes)
         {
+            //todo
             NetworkEvent ne = new NetworkEvent();
 
             return ne;
