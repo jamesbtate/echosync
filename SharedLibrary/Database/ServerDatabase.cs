38fc7d2c3cd5cbb9745b2fa9dcbb001706fc0058
jamesbtate <jamesbtate@gmail.com>
2013-07-07 13:09:45 -0400

    Added MySQL reference.

---
+++ SharedLibrary/Database/ServerDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace SharedLibrary.Database
+{
+    class ServerDatabase
+    {
+
+    }
+}
