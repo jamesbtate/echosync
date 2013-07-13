63d967a85fac3d738bcbd2d4b9596c31233ef466
jamesbtate <jamesbtate@gmail.com>
2013-07-13 08:49:22 -0400

    added Computer table to brownie's MySQL database and expanded ER diagram for server database. Added a tiny bit of DB code to ServerDatabase.cs

--- SharedLibrary/Database/ServerDatabase.cs
+++ SharedLibrary/Database/ServerDatabase.cs
@@ -7,8 +7,22 @@ using MySql.Data.MySqlClient;
 
 namespace SharedLibrary.Database
 {
-    class ServerDatabase
+    public static class ServerDatabase
     {
-
+        static String connString = "server=128.82.4.29;uid=echosync;pwd=yjPLAjAqu13lVO4l1dw7G74D;database=echosync;";
+        MySqlConnection connection;
+        
+        void Init()
+        {
+            try
+            {
+                connection = new MySqlConnection(connString);
+                connection.Open();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
